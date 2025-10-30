1d65a048a5e39c5bd03adba6b074f04395217c1b
DevyatovEgor <devtyatovegor2004@gmail.com>
2025-10-30 20:46:23 +0500

    Update DataService.cs

--- Tyuiu.DevyatovEV.Sprint3.Task0.V29.Lib/DataService.cs
+++ Tyuiu.DevyatovEV.Sprint3.Task0.V29.Lib/DataService.cs
@@ -1,23 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// DataService.cs
+using System;
 using tyuiu.cources.programming.interfaces.Sprint3;
 
-namespace Tyuiu.DevyatovEV.Sprint3.Task0.V29.Lib
+namespace Tyuiu.DevyatovEV.Sprint3.Task5.V11.Lib
 {
-    public class DataService : ISprint3Task0V29
+    public class DataService : ISprint3Task5V11
     {
-        public double GetSumSeries(double value, int startValue, int stopValue)
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
             double sumSeries = 0;
 
-            for (int k = startValue; k <= stopValue; k++)
+            // Внешний цикл по i от startValue1 до stopValue1
+            for (int i = startValue1; i <= stopValue1; i++)
             {
-                double term = (Math.Pow(value, 2 * k) + (1.0 / (k + 1))) * Math.Cos(value);
-                sumSeries += term;
+                // Внутренний цикл по k от startValue2 до stopValue2
+                for (int k = startValue2; k <= stopValue2; k++)
+                {
+                    sumSeries += Math.Sin(k);
+                }
             }
+
+            // Добавляем x/2 для каждого элемента двойной суммы
+            // Количество элементов = (stopValue1 - startValue1 + 1) * (stopValue2 - startValue2 + 1)
+            int count = (stopValue1 - startValue1 + 1) * (stopValue2 - startValue2 + 1);
+            sumSeries += count * (x / 2.0);
+
             return Math.Round(sumSeries, 3);
         }
     }
