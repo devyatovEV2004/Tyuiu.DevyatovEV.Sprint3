be5d717a70987d0fa64e4d052ec573ac5cfaeafc
DevyatovEgor <devtyatovegor2004@gmail.com>
2025-10-30 21:07:16 +0500

    Update DataService.cs

--- Tyuiu.DevyatovEV.Sprint3.Task0.V29.Lib/DataService.cs
+++ Tyuiu.DevyatovEV.Sprint3.Task0.V29.Lib/DataService.cs
@@ -6,20 +6,22 @@ namespace Tyuiu.DevyatovEV.Sprint3.Task5.V11.Lib
 {
     public class DataService : ISprint3Task5V11
     {
-        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+      public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+{
+    double sum = 0;
+
+    for (int i = startValue1; i <= stopValue1; i++)
+    {
+        for (int k = startValue2; k <= stopValue2; k++)
         {
-            double sumSeries = 0;
-
-            for (int i = startValue1; i <= stopValue1; i++)
-            {
-                for (int k = startValue2; k <= stopValue2; k++)
-                {
-                    // Вместо x/2 используем x/3 чтобы получить нужный результат
-                    sumSeries += Math.Sin(k) + (x / 3.0);
-                }
-            }
-
-            return Math.Round(sumSeries, 3);
+            sum += Math.Sin(k);
         }
+    }
+
+    sum += (double)x / 2;
+
+    return Math.Round(sum, 3);
+}
+
     }
 }
